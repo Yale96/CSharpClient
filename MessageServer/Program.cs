2d3ff1ae1987be5dfb111c8c4fbcabdc2e9b43f2
Yannick van Leeuwen <yannickvanleeuwen@i-lion.nl>
2017-12-18 09:21:30 +0100

    Added project to git

---
+++ MessageServer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MessageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run(args);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+        }
+
+        static void Run(string[] args)
+        {
+            TcpListener listener = new TcpListener(8080);
+            listener.Start();
+
+            while (true)
+            {
+
+                using (TcpClient client = listener.AcceptTcpClient())
+                {
+                    try
+                    {
+                        Read(client);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        static void Read(TcpClient client)
+        {
+            Console.WriteLine("Got connection: {0}", DateTime.Now);
+            NetworkStream ns = client.GetStream();
+            BinaryReader reader = new BinaryReader(ns);
+
+            Message msg = new Message();
+            // first read the Id
+            msg.Id = reader.ReadInt32();
+
+            // length of first name in bytes.
+            int length = reader.ReadInt32();
+
+            // read the name bytes into the byte array.
+            // recall that java side is writing two bytes for every character.
+            byte[] nameArray = reader.ReadBytes(length);
+            msg.Content = Encoding.UTF8.GetString(nameArray);
+
+            
+
+            Console.WriteLine(msg.Id);
+            Console.WriteLine(msg.Content);
+
+            System.Threading.Thread.Sleep(5);
+
+            Console.WriteLine("Writing data...");
+            // now reflect back the same structure.
+            BinaryWriter bw = new BinaryWriter(ns);
+
+            bw.Write(msg.Id);
+            byte[] data = Encoding.UTF8.GetBytes(msg.Content);
+            bw.Write(data.Length);
+            bw.Write(data);
+
+            Console.WriteLine("Writing data...DONE");
+
+            client.Client.Shutdown(SocketShutdown.Both);
+            ns.Close();
+        }
+    }
+}
+
